3ae04bbd7d5e52f5db46e56aaecb65eea9cf227a
kanaami <boyjar.server@gmail.com>
2017-08-09 19:10:03 +0900

    ウィザスケの頭も送れるように

--- src/com/plugin/ftb/witherhunt/FutabaCommand.java
+++ src/com/plugin/ftb/witherhunt/FutabaCommand.java
@@ -19,23 +19,25 @@ public class FutabaCommand implements CommandExecutor {
 		
 		if(sender instanceof Player) {
 			Player player = (Player)sender;
-			if(Bukkit.getPlayerExact("ftb28") == null) {
+			if(Bukkit.getPlayerExact("kanaami") == null) {
 				player.sendMessage(ChatColor.RED + "ftb28さんはいません");
 				return true;
 			}
-			Player ftb28 = Bukkit.getPlayerExact("ftb28");
+			Player ftb28 = Bukkit.getPlayerExact("kanaami");
 			for(ItemStack itemStack : player.getInventory().getContents()) {
 				if(itemStack != null) {
-					if(itemStack.getType() == Material.BLAZE_ROD || itemStack.getType() == Material.ENDER_PEARL) {
-						int amount = itemStack.getAmount();
+					if(itemStack.getType() == Material.BLAZE_ROD || itemStack.getType() == Material.ENDER_PEARL
+							|| itemStack.getType().equals(Material.SKULL_ITEM) && itemStack.getDurability() == 1) {
 						Material item = itemStack.getType();
-						ftb28.getInventory().addItem(new ItemStack(item, amount));
+						ftb28.getInventory().addItem(itemStack);
 						String name;
 						if(item == Material.BLAZE_ROD) {
 							name = "ブレイズロッド";
 						}
 						else if(item == Material.ENDER_PEARL) {
 							name ="エンダーパール";
+						}else if(item == Material.SKULL_ITEM) {
+							name = "ウィザースケルトンの頭";
 						}
 						else {
 							name = "null";
