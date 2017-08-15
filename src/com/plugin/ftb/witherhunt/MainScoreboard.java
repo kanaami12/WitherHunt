e92144b0808fd868148104b1a6ae6e3d8ff5ffb6
rushoko <30364245+rushoko@users.noreply.github.com>
2017-08-15 16:30:45 +0900

    スコアボードに地下の人地上の人を表示するようにしました(バグあり)

--- src/com/plugin/ftb/witherhunt/MainScoreboard.java
+++ src/com/plugin/ftb/witherhunt/MainScoreboard.java
@@ -1,16 +1,14 @@
 package com.plugin.ftb.witherhunt;
 
-import java.util.UUID;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
-import org.bukkit.scoreboard.Score;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class MainScoreboard {
@@ -18,6 +16,8 @@ public class MainScoreboard {
 	public static Main plugin = Main.plugin;
 	
 	public static int numberOfNether = 0;
+	public static int numberOfHigher = 0;
+	public static int numberOfLower = 0;
 	
 	public static World nether;
 	
@@ -38,6 +38,8 @@ public class MainScoreboard {
 			
 			//オブジェクトの登録
 			object.getScore("" + ChatColor.RED + ChatColor.BOLD + "ネザーの人  " + ChatColor.RESET + ":   ").setScore(numberOfNether);
+			object.getScore("" + ChatColor.RED + ChatColor.BOLD + "地下の人  " + ChatColor.RESET + ":   ").setScore(numberOfLower);
+			object.getScore("" + ChatColor.RED + ChatColor.BOLD + "地上の人  " + ChatColor.RESET + ":   ").setScore(numberOfHigher);
 		}
 		
 		//ネザーワールドを取得
@@ -64,7 +66,11 @@ public class MainScoreboard {
 		Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
 		Objective object = board.getObjective("NumberOfPeople");
 		
+		setNumber();
+		
 		object.getScore("" + ChatColor.RED + ChatColor.BOLD + "ネザーの人  " + ChatColor.RESET + ":   ").setScore(nether.getPlayers().size());
+		object.getScore("" + ChatColor.RED + ChatColor.BOLD + "地下の人  " + ChatColor.RESET + ":   ").setScore(numberOfLower);
+		object.getScore("" + ChatColor.RED + ChatColor.BOLD + "地上の人  " + ChatColor.RESET + ":   ").setScore(numberOfHigher);
 	}
 	
 	//スコアボードをプレイヤーにセット
@@ -72,5 +78,23 @@ public class MainScoreboard {
 		Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
 		Objective object = board.getObjective("NumberOfPeople");
 		player.setScoreboard(board);
+	}
+	
+	//地下、地上の人の人数を数える
+	public static void setNumber() {
+		
+		numberOfHigher = 0;
+		numberOfLower = 0;
+		
+		for(Player player : Bukkit.getOnlinePlayers()) {
+			if(player.getLocation().getBlock().getBiome() != Biome.HELL) {
+				if(player.getLocation().getBlockY() < 62) {
+					numberOfLower += 1;
+				}
+				else {
+					numberOfHigher += 1;
+				}
+			}
+		}
 	}
 }
